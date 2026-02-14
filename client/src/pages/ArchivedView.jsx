6a042944a4bd872aeabf709c927ec2f19aed5afa
Oluwabusola Adesoji <bankeadesoji@gmail.com>
2026-02-14 20:53:12 +0100

    Fix archived resources page by adding missing React imports

--- client/src/pages/ArchivedView.jsx
+++ client/src/pages/ArchivedView.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { Loader2, ArrowLeft } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import ResourceCard from '../components/resources/ResourceCard';
